bf0d907c9d589096fa7cdc9c32d774bd444504ad
corieprater93 <corieprater@gmail.com>
2026-07-16 22:32:13 -0500

    Creating Joke Add, Update, and Delete Views

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,24 @@
-from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView   
+)
 from .models import Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer',]
+
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
 
 class JokeDetailView(DetailView):
     model = Joke
 
 class JokeListView(ListView):
     model = Joke
+
+class JokeUpdatedView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
