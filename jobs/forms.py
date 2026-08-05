15123c73b799b518d57a17c44c7e3a6d77034230
corieprater93 <corieprater@gmail.com>
2026-08-05 12:13:51 -0500

    Improving the Job Application form with widgets.

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django import forms
 
 class JobsApplicationForm(forms.Form):
@@ -16,20 +17,66 @@ class JobsApplicationForm(forms.Form):
         (5, 'FRI'), 
     )
 
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    website = forms. URLField(required=False)
+    first_name = forms.CharField(
+        widget=forms.URLInput(
+            attrs={
+                    'autofocus': True,
+                    'class': 'form.control',
+                }
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.URLInput(
+            attrs={
+                'class': 'form.control',
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.URLInput(
+                    attrs={
+                        'class': 'form.control',
+                    }
+                )
+    )
+    website = forms.URLField(
+        required=False,
+                widget=forms.URLInput(
+                    attrs={
+                    'size': '50',
+                    'class': 'form-control',
+                    'placeholder': 'https://www.example.com',
+                    }
+                )
+    )
+        
     employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
+
+    # Start date section
+    YEARS = range(datetime.now().year, datetime.now().year+2)
+
     start_date = forms.DateField(
-        help_text='The earliest date you can start working (January 1, 2000).'
+        help_text="The earliest you can start working.",
+        widget=forms.SelectDateWidget(
+            years=YEARS, 
+        )
     )
-    avaliable_days = forms.MultipleChoiceField(
+    avaliable_days = forms.TypedMultipleChoiceField(
         choices=DAYS,
-        help_text='Select all days that you can work.'
+        coerce=int,
+        help_text="Check all days that you can work.",
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked':True}
+        )
     )
-    desired_hourly_wage = forms.DecimalField()
-    cover_letter = forms.CharField()
+    desired_hourly_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={'min':'10.00', 'max':'100.00', 'step':'.25'}
+        )
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea(attrs={'cols':'75', 'rows': '5'})
+    )
     confirmation = forms.BooleanField(
         label='I certify that the information I have provided is true.'
     )
