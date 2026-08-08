6ef77104df0e8e210d274bffc734124b3a479936
corieprater93 <corieprater@gmail.com>
2026-08-08 17:06:18 -0500

    Creating tags

--- jokes/models.py
+++ jokes/models.py
@@ -6,8 +6,8 @@ from common.utils.text import unique_slug
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
-
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    tags = models.ManyToManyField('Tag', blank=True)
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
@@ -27,6 +27,7 @@ class Joke(models.Model):
     def __str__(self):
         return self.question
 
+
 class Category(models.Model):
     category = models.CharField(max_length=50)
     slug = models.SlugField(
@@ -36,7 +37,7 @@ class Category(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
-        return reverse('joke:category', args=[self.slug])
+        return reverse('jokes:category', args=[self.slug])
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -48,4 +49,27 @@ class Category(models.Model):
         return self.category
 
     class Meta:
-            verbose_name_plural = 'Categories'
+        verbose_name_plural = 'Categories'
+        ordering = ['category']
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:tag', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        ordering = ['tag']
