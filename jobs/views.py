f4013a0126022b232522fc3e18417901dff5ba31
corieprater93 <corieprater@gmail.com>
2026-08-08 22:10:45 -0500

    Tying Job Appliations to Models

--- jobs/views.py
+++ jobs/views.py
@@ -1,13 +1,15 @@
 import html
 from django.urls import reverse_lazy
-from django.views.generic import FormView, TemplateView
+from django.views.generic import CreateView, TemplateView
 
 from common.utils.email import send_email
-from .forms import JobsApplicationForm
 
-class JobAppView(FormView):
-    template_name = 'jobs/joke_writer.html'
-    form_class = JobsApplicationForm
+from .models import Applicant
+from .forms import JobApplicationForm
+
+class JobAppView(CreateView):
+    model = Applicant
+    form_class = JobApplicationForm
     success_url = reverse_lazy('jobs:thanks')
 
     def form_valid(self, form):
@@ -15,12 +17,12 @@ class JobAppView(FormView):
         to = 'corieprater@gmail.com'
         subject = 'Application for Joke Writer'
         content = f'''<p>Hey HR Manager!</p>
-            <p>Job application recieved:<p>
+            <p>Job application received:</p>
             <ol>'''
         for key, value in data.items():
             label = key.replace('_', ' ').title()
             entry = html.escape(str(value), quote=False)
-            content += f'<li>{label}: {entry}'
+            content += f'<li>{label}: {entry}</li>'
         
         content += '</ol>'
 
